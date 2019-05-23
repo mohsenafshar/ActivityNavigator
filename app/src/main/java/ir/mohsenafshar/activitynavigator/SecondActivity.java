b1d5edbcf5d7e8fccb496d230452e0b235cf6392
mohsen afshar <mohsenafshar_ph@yahoo.com>
2019-05-23 09:15:32 +0430

    initial commit

---
+++ app/src/main/java/ir/mohsenafshar/activitynavigator/SecondActivity.java
@@ -0,0 +1,25 @@
+package ir.mohsenafshar.activitynavigator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import ir.mohsenafshar.Navigator;
+import ir.mohsenafshar.navigatorannotation.NavigatorAnnotation;
+
+@NavigatorAnnotation
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String TAG = "SECONDACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            Log.d(TAG, "onCreate: " + extras.get("KEY"));
+        }
+    }
+}
